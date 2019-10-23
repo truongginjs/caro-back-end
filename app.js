0d3684566a1f9db50c7fd14efeb2222e8ebb35b5
truong nguyen <44205504+truongginjs@users.noreply.github.com>
2019-10-23 22:58:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors =require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -26,25 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Add headers
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "https://caro-2.herokuapp.com");
-
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(cors());
 
 app.use('/user', usersRouter);
 app.use('/me', passport.authenticate('jwt', { session: false }), meRouter);
